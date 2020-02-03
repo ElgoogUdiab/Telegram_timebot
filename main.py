b732098876f32b89b208f0d66b0d3cac2feba6e4
ElgoogUdiab <ElgoogUdiab@users.noreply.github.com>
2020-02-03 17:50:19 +0800

    Update mention and text_mention

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 DEFAULT_TIMEZONE = 'UTC'
-fmt="%Y-%m-%d %H:%M:%S %Z%z"
+fmt="%Y-%m-%d %H:%M:%S"
 
 # Timezone tree generation
 tz_dict = {}
@@ -60,6 +60,15 @@ try:
 except:
     timezones={}
 
+try:
+    with open("id_name.json") as f:
+        temp = json.load(f)
+        id_name=temp["id_name"]
+        name_id=temp["name_id"]
+except:
+    id_name={}
+    name_id={}
+
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
@@ -70,10 +79,24 @@ def update_timezone(chat_id, timezone):
     with open("timezone.json", "w") as f:
         json.dump(timezones, f)
 
+def update_user(name, new_id):
+    new_id = str(new_id)
+    if name in name_id:
+        old_id = name_id[name]
+        id_name.pop(old_id)
+    name_id[name] = new_id
+    id_name[new_id] = name
+    with open("id_name.json", "w") as f:
+        json.dump({"id_name": id_name, "name_id": name_id}, f)
+
 def start(update, context):
     if update.message.chat.type == "private":
-        update_timezone(str(update.effective_user.id), DEFAULT_TIMEZONE)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"The default timezone is {DEFAULT_TIMEZONE}.\nPlease set your own timezone with /settz")
+        if update.effective_user.username != None and str(update.effective_user.id) in timezones:
+            update_user(update.effective_user.username,update.effective_user.id)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"User name infomation updated.")
+        else:
+            update_timezone(str(update.effective_user.id), DEFAULT_TIMEZONE)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"The default timezone is {DEFAULT_TIMEZONE}.\nPlease set your own timezone with /settz")
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
@@ -171,7 +194,7 @@ def custom(update, context):
     )
     context.bot.send_message(
         chat_id = context.user_data["message"].chat_id,
-        text = f"Timezone set as {tzname}.",
+        text = f"Timezone set as {timezone}.",
         reply_markup = None
     )
     context.user_data.clear()
@@ -195,13 +218,24 @@ def get_time(update, context):
     if len(context.args) != 1:
         context.bot.send_message(chat_id=update.effective_chat.id, text=f"Usage: /time <@user>")
         return
-    # context.bot.send_message(chat_id=update.effective_chat.id, text=str(update.message.parse_entities()))
-    for entity in update.message.parse_entities():
-        if entity.type=="mention":
-            print(entity)
-            tz = pytz.timezone(timezones[str(entity.user.id)])
+    
+    for entity, string in update.message.parse_entities().items():
+        if entity.type.endswith("mention"):
+            if entity.type=="text_mention":
+                userid = str(entity.user.id)
+            elif entity.type=="mention":
+                username = string[1:]
+                try:
+                    userid = name_id[username]
+                except:
+                    update.message.reply_text(f"I don't know that guy! Let him/her /start with me!")
+                    return
+            if not userid in timezones:
+                update.message.reply_text(f"I don't know that guy's timezone! Let him/her /start with me!")
+                return
+            tz = pytz.timezone(timezones[userid])
             time_now = datetime.utcnow().replace(tzinfo=pytz.utc)
-            update.message.reply(f"The time for him/her is {time_now.astimezone(tz).strftime(fmt)}")
+            update.message.reply_text(f"The time for him/her is {time_now.astimezone(tz).strftime(fmt)}")
             return
 get_time_handler = CommandHandler("time", get_time, pass_args=True)
 dispatcher.add_handler(get_time_handler)
