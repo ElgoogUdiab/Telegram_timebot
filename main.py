d3526e842072fe38fcfb9c73ef5722b9656b1501
ElgoogUdiab <ElgoogUdiab@users.noreply.github.com>
2020-02-03 15:30:26 +0800

    Timezone select

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater
 import json
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler, ConversationHandler, Filters
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 import logging
 import pytz
@@ -12,9 +12,39 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_TIMEZONE = 'UTC'
 
-tz_keyboard = [
-    [KeyboardButton(tzname)] for tzname in pytz.all_timezones
-]
+# Timezone tree generation
+tz_dict = {}
+depth = 0
+for tzname in pytz.common_timezones:
+    tzname = tzname.replace("_", " ")
+    tz_split = tzname.split("/")
+    depth = max(depth, len(tz_split))
+    curr_dict = tz_dict
+    for name in tz_split:
+        if not name in curr_dict:
+            curr_dict[name] = {}
+        curr_dict = curr_dict[name]
+
+PREVIOUS_TEXT = "<< Go Back"
+CUSTOM_TEXT = "Custom timezone"
+def gen_markup(prefix=[]):
+    curr_dict = tz_dict
+    for name in prefix:
+        curr_dict = curr_dict[name]
+    keyboard = [
+        [KeyboardButton(name)] for name in curr_dict.keys()
+    ]
+    if len(prefix) == 0:
+        last_line = [
+            KeyboardButton(CUSTOM_TEXT)
+        ]
+    else:
+        last_line = [
+            KeyboardButton(PREVIOUS_TEXT),
+            KeyboardButton(CUSTOM_TEXT)
+        ]
+    keyboard.append(last_line)
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
 
 try:
     with open("token") as f:
@@ -40,20 +70,97 @@ def update_timezone(chat_id, timezone):
 def start(update, context):
     if update.message.chat.type == "private":
         update_timezone(str(update.effective_user.id), DEFAULT_TIMEZONE)
-        context.bot.send_message(chat_id=update.effective_user.id, text=f"The default timezone is {DEFAULT_TIMEZONE}.\nPlease set your own timezone with /settz")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"The default timezone is {DEFAULT_TIMEZONE}.\nPlease set your own timezone with /settz")
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+SELECT, CUSTOM = range(2)
 def set_tz(update, context):
     if update.message.chat.type != "private":
         update.message.reply("Only use this command in private message!")
+        return ConversationHandler.END
     else:
-        reply_markup = ReplyKeyboardMarkup(tz_keyboard)
-        context.bot.send_message(chat_id=update.effective_user.id, text=f"Please select the timezone below.", reply_markup=reply_markup)
-
-language_handler = CommandHandler('settz', set_tz)
-dispatcher.add_handler(language_handler)
-
+        message = context.bot.send_message(chat_id=update.effective_chat.id, text=f"Please select the timezone below.\nType /cancel anytime to cancel.", reply_markup=gen_markup())
+        context.user_data["tz"] = []
+        context.user_data["message"] = message
+        return SELECT
+def select(update, context):
+    region = update.message.text
+    if region == CUSTOM_TEXT:
+        context.bot.delete_message(
+            chat_id = context.user_data["message"].chat_id,
+            message_id = context.user_data["message"].message_id,
+        )
+        message = context.bot.send_message(
+            chat_id = context.user_data["message"].chat_id,
+            text = "Type the timezone name below."
+        )
+        context.user_data["message"]=message
+        return CUSTOM
+    elif region == PREVIOUS_TEXT:
+        context.user_data["tz"].pop()
+        context.bot.delete_message(
+            chat_id = context.user_data["message"].chat_id,
+            message_id = context.user_data["message"].message_id,
+        )
+        message = context.bot.send_message(
+            chat_id = context.user_data["message"].chat_id,
+            text="Please select the timezone below.\nType /cancel anytime to cancel.",
+            reply_markup = gen_markup(context.user_data["tz"])
+        )
+        context.user_data["message"]=message
+        return SELECT
+    curr_dict = tz_dict
+    for name in context.user_data["tz"]:
+        curr_dict = curr_dict[name]
+    if not region in curr_dict:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="No such zone! Please select the zone again.")
+        return SELECT
+    curr_dict = curr_dict[region]
+    context.user_data["tz"].append(region)
+    if curr_dict == {}:
+        tzname = "/".join(context.user_data["tz"])
+        context.bot.delete_message(
+            chat_id = context.user_data["message"].chat_id,
+            message_id = context.user_data["message"].message_id
+        )
+        context.bot.send_message(
+            chat_id = context.user_data["message"].chat_id,
+            text = f"Timezone set as {tzname}.",
+            reply_markup = None
+        )
+        update_timezone(str(update.effective_user.id), tzname)
+        context.user_data.clear()
+        return ConversationHandler.END
+    else:
+        context.bot.delete_message(
+            chat_id = context.user_data["message"].chat_id,
+            message_id = context.user_data["message"].message_id,
+        )
+        message = context.bot.send_message(
+            chat_id = context.user_data["message"].chat_id,
+            text="Please select the timezone below.\nType /cancel anytime to cancel.",
+            reply_markup = gen_markup(context.user_data["tz"])
+        )
+        context.user_data["message"]=message
+        return SELECT
+def custom(update, context):
+    pass
+    return ConversationHandler.END
+def cancel(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"OK! Canceled")
+    data = context.user_data
+    data.clear()
+    return ConversationHandler.END
+select_tz_handler = ConversationHandler(
+    entry_points = [CommandHandler("settz", set_tz)],
+    states = {
+        SELECT: [MessageHandler(Filters.text, select)],
+        CUSTOM: [MessageHandler(Filters.text, custom)]
+    },
+    fallbacks = [CommandHandler('cancel', cancel)]
+)
+dispatcher.add_handler(select_tz_handler)
 
 def error(update, context):
     """Log Errors caused by Updates."""
